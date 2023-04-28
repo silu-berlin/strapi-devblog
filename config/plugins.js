af9ed1f8f8878ea52b2f1bc491f5555bce734fbb
Mario Adler-Kuczera <kontakt@silu-berlin.de>
2023-04-28 17:16:23 +0200

    add cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,40 @@
-module.exports = {
-    // ...
+module.exports = ({env})  => ({
     // 'github-projects': {
     //     enabled: false,
     //     resolve: './src/plugins/github-projects'
     // },
-    // ...
-}
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: env('SMTP_HOST', 'smtp.example.com'),
+                port: env('SMTP_PORT', 587),
+                auth: {
+                    user: env('SMTP_USERNAME'),
+                    pass: env('SMTP_PASSWORD'),
+                },
+                secure: true,
+                // ... any custom nodemailer options
+            },
+            settings: {
+                defaultFrom: 'kontakt@silu-berlin.de',
+                defaultReplyTo: 'kontakt@silu-berlin.de',
+            },
+        },
+    },
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+})
